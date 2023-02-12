b43773dae7b3c881068505b3e23b40f098908674
engage_intelllect <61082194+engageintellect@users.noreply.github.com>
2023-02-11 23:14:35 -0800

    Initial commit

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,16 @@
+<script lang="ts">
+	import { enhance, type SubmitFunction } from '$app/forms';
+	import { page } from '$app/stores';
+	import '$lib/app.css';
+	import Nav from '$lib/components/Nav.svelte';
+	import Footer from '$lib/components/Footer.svelte';
+</script>
+
+<div class="bg-base-200">
+	<Nav />
+
+	<div class="">
+		<slot />
+	</div>
+	<Footer />
+</div>
